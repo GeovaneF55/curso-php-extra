1f5a8be5d97fc8c07d8bb09659e626c17c3647f9
GeovaneF55 <geovanefss@gmail.com>
2022-04-23 21:12:38 +0100

    Fundamentos javascript

---
+++ javascript/fundaments/comment.js
@@ -0,0 +1,15 @@
+// comentário de uma linha
+console.log('Linha 1')
+
+/*
+  comentário de
+  múltiplas linhas
+*/
+console.log('Linha 2')
+
+/*
+ * comentário de
+ * múltiplas linhas
+ * com asterisco
+ */
+console.log('Linha 3')
---
+++ javascript/fundaments/organization.js
@@ -0,0 +1,11 @@
+console.log('Sentença de código');
+
+{
+  console.log('Sentença de código em bloco 1')
+  {
+    console.log('Sentença de código em bloco dentro de bloco 1')
+    console.log('Sentença de código em bloco dentro de bloco 2')
+  }
+  console.log('Sentença de código em bloco 2')
+  console.log('Sentença de código em bloco 3')
+}
