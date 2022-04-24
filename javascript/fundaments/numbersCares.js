49fa435ef43b7bedfe75ee9190f53e049c72b9ba
GeovaneF55 <geovanefss@gmail.com>
2022-04-24 17:18:03 +0100

    Javascript - Números Alguns Cuidados

---
+++ javascript/fundaments/numbersCares.js
@@ -0,0 +1,6 @@
+console.log(7 / 0)                // Infinity
+console.log("10" / 2)             // 5
+console.log("Show!" * 2)          // NaN
+console.log(0.1 + 0.7)            // 0.7999999999999999
+// console.log(10.toString())     // SyntaxError
+console.log((10.345).toFixed(2))  // 10.35
