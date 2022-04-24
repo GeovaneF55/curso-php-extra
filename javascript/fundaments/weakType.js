2c4fc3799739edc9e9ebabe6ccfa1f165b848808
GeovaneF55 <geovanefss@gmail.com>
2022-04-24 16:57:24 +0100

    Javascript - Tipagem Fraca

---
+++ javascript/fundaments/weakType.js
@@ -0,0 +1,12 @@
+let any = 'Legal'
+console.log(any)
+console.log(typeof any)
+
+any = 3.1516  
+console.log(any)
+console.log(typeof any)
+
+// Evite nome genéricos e siglas
+let value = ''
+let number = 1
+let pqp = false // Produto Químico Perigoso...kkk
