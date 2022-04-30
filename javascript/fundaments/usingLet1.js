03dfde312f33d6c08434f506ce02c4cca46fb7af
GeovaneF55 <geovanefss@gmail.com>
2022-04-30 19:38:52 +0100

    Javascript - Declaração de Variáveis Com Let

---
+++ javascript/fundaments/usingLet1.js
@@ -0,0 +1,26 @@
+// Teste 1
+
+var num = 1
+{
+  let num = 2
+  console.log("dentro 1 = ", num)
+}
+console.log("fora 1 = ", num)
+
+// Teste 2
+
+let num2 = 1
+{
+  let num2 = 2
+  console.log("dentro 2 = ", num2)
+}
+console.log("fora 2 = ", num2)
+
+// Teste 3
+
+let num3 = 1
+{
+  let num4 = 2
+  console.log("dentro 3 = ", num3)
+}
+console.log("fora 3 = ", num3)
