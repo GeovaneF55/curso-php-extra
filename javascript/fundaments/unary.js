e0fe6cd5c4526959ff49b9832ed90be4a2241c88
GeovaneF55 <geovanefss@gmail.com>
2022-05-05 18:46:15 +0100

    Javascript - Operadores Unários

---
+++ javascript/fundaments/unary.js
@@ -0,0 +1,11 @@
+let num1 = 1
+let num2 = 2
+
+num1++   // Incrementa o valor de num1
+console.log(num1)
+
+--num1   // Decrementa o valor de num1
+console.log(num1)
+
+console.log(++num1 === num2--) // Incrementa o valor de num1 e compara com o valor de num2
+console.log(num1 === num2)   // Compara o valor de num1 com o valor de num2
