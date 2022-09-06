12d2529a277edde1b74312d84fb347c14324ba6f
GeovaneF55 <geovanefss@gmail.com>
2022-09-06 19:33:50 +0100

    Banco Relacional - Inserir Estados

---
+++ banco-relacional/inserirEstados1.sql
@@ -0,0 +1,8 @@
+insert into estados (nome, sigla, regiao, populacao)
+values ('Acre', 'AC', 'Norte', 0.83);
+
+insert into estados (nome, sigla, regiao, populacao)
+values
+    ('Alagoas', 'AL', 'Norte', 3.38),
+    ('Amapá', 'AP', 'Norte', 0.8),
+    ('Amazonas', 'AM', 'Norte', 4.06);
