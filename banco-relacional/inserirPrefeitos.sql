f716e9c0e9e2854fb766aa58f70e06299af54f2b
GeovaneF55 <geovanefss@gmail.com>
2022-09-08 21:58:29 +0100

    Banco Relacional - Inserir Prefeitos

---
+++ banco-relacional/inserirPrefeitos.sql
@@ -0,0 +1,20 @@
+select * from cidades;
+
+insert into prefeitos
+    (nome, cidade_id)
+values
+    ('Rodrigo Neves', 2),
+    ('Raquel Lyra', 3),
+    ('Zenaldo Coutinho', null);
+
+select * from prefeitos;
+
+insert into prefeitos
+    (nome, cidade_id)
+values
+    ('Rafael Greca', null);
+
+insert into prefeitos
+    (nome, cidade_id)
+values
+    ('Rodrigo Pinheiro', 3);
