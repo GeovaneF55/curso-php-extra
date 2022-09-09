22c5937fb97eb05c24ae7d319e18443248b4fe0f
GeovaneF55 <geovanefss@gmail.com>
2022-09-09 11:45:43 +0100

    Banco Relacional - Explorando os Tipos de JOINs

---
+++ banco-relacional/consultarComJoins.sql
@@ -0,0 +1,39 @@
+select * from prefeitos;
+
+select * from cidades;
+
+select * from cidades c
+inner join prefeitos p
+on c.id = p.cidade_id;
+
+select * from cidades c
+left outer join prefeitos p
+on c.id = p.cidade_id;
+
+select * from cidades c
+right outer join prefeitos p
+on c.id = p.cidade_id;
+
+select * from cidades c
+full join prefeitos p
+on c.id = p.cidade_id;
+
+select * from cidades c
+left outer join prefeitos p
+on c.id = p.cidade_id
+
+union
+
+select * from cidades c
+right outer join prefeitos p
+on c.id = p.cidade_id;
+
+select * from cidades c
+left outer join prefeitos p
+on c.id = p.cidade_id
+
+union all
+
+select * from cidades c
+right outer join prefeitos p
+on c.id = p.cidade_id;
